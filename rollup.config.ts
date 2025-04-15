90fe48843e244f52751bb94265308ff9b21f63bb
Jan Storm <jstorm@die-netzwerkstatt.de>
2025-04-15 15:16:46 +0200

    task: compile for use with es5

--- rollup.config.ts
+++ rollup.config.ts
@@ -6,23 +6,18 @@ import external from "rollup-plugin-peer-deps-external";
 import progress from "rollup-plugin-progress";
 import { terser } from "rollup-plugin-terser";
 import typescript from "rollup-plugin-typescript2";
+import babel from '@rollup/plugin-babel';
 
 export default {
   input: "src/index.ts",
   output: [
     {
-      // CommonJS Output
-      file: "./dist/index.js",
-      format: "cjs",
-      exports: "named",
-      sourcemap: true,
-    },
-    {
       // Browser / ES6 output
-      file: "./dist/index.es.js",
-      format: "es",
+      file: "./dist/leaflet.locales.js",
+      format: "iife",
       exports: "named",
       sourcemap: true,
+      name: "leafletLocales",
     },
   ],
   plugins: [
@@ -33,10 +28,15 @@ export default {
     }),
     progress(),
     external(),
-    resolve(),
+    resolve({browser: true}),
     typescript({
       rollupCommonJSResolveHack: true,
       clean: true,
+    }),
+    babel({ // Transpile to ES5
+      babelHelpers: 'bundled',
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      exclude: 'node_modules/**'
     }),
     commonjs(),
     terser({
